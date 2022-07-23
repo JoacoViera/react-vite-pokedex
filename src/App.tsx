c24a9c1b056434e32e68656ce2f32406d5a078b5
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-22 23:15:57 -0300

    unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex items-center bg-black h-screen justify-center">
+      <h1 className=" text-white text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
