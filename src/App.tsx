95401327e4eec9a653d2b8c7bb9f7e5f1de95f68
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-23 05:02:30 -0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PokemonPage from "./Pages/PokemonPage";
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<div className="flex items-center bg-black h-screen justify-center">
-				<h1 className="text-white text-3xl font-bold underline">
-					Hello world!
-				</h1>
-
-				<div />
+			<div className="flex justify-center items-center bg-slate-900 h-screen">
+				<PokemonPage />
 			</div>
 		</QueryClientProvider>
 	);
