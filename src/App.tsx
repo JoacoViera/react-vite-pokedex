6a2bf2642175e7e193104fdbe77f55302a99dfa9
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-23 03:01:02 -0300

    eslint airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, {  } from 'react'
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function App () {
+const queryClient = new QueryClient();
 
-  return (
-    <div className="flex items-center bg-black h-screen justify-center">
-      <h1 className=" text-white text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
+function App() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<div className="flex items-center bg-black h-screen justify-center">
+				<h1 className="text-white text-3xl font-bold underline">
+					Hello world!
+				</h1>
+
+				<div />
+			</div>
+		</QueryClientProvider>
+	);
 }
 
-export default App
+export default App;
