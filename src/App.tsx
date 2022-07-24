07ae989720c25ded5c696f36773baf16c4ed2822
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-24 12:40:41 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
     <div className="flex flex-col bg-slate-900 h-screen">
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <nav className="border-gray-200 dark:bg-gray-900">
-            <div className="justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+          <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
               <Link to="react-vite-pokedex/" className="flex items-center">
                 <img
-                  src="../assets/pokedex.png"
+                  src="https://img.icons8.com/color/480/pokedex.png"
                   className="mr-3 h-6 sm:h-9"
                   alt="Pokedex Logo"
                 />
@@ -48,11 +48,8 @@ function App() {
           </nav>
           <nav className="bg-gray-50 dark:bg-gray-700">
             <div className="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
-              <div
-                className="hidden w-full md:block md:w-auto"
-                id="navbar-default"
-              >
-                <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <div className="flex items-center">
+                <ul className="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
                   <Link to="react-vite-pokedex/">
                     <p className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                       List
