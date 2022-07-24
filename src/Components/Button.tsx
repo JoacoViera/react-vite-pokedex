71149fa7c90943b799dbb253f0a67e1d0d33ea26
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-24 10:54:46 -0300

    new button prop

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-export default function Button({ onClick = () => {}, title = "" }: any) {
+export default function Button({
+  onClick = () => {},
+  title = "",
+  disabled = false,
+}: any) {
   return (
     <button
       type="button"
       className="min-w-[6%] text-white mt-10 right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       onClick={onClick}
+      disabled={disabled}
     >
       {title}
     </button>
